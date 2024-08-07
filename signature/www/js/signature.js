d4225afd6e1b406a3f81b36ae7868b12dd072025
Arthur Bréant <arthur@thinkr.fr>
2024-08-07 18:10:51 +0200

    feat: create dark mode

--- signature/www/js/signature.js
+++ signature/www/js/signature.js
@@ -1,5 +1,36 @@
+function setTheme(mode = "auto") {
+  const userMode = localStorage.getItem("bs-theme");
+  const sysMode = window.matchMedia("(prefers-color-scheme: light)").matches;
+  const useSystem = mode === "system" || (!userMode && mode === "auto");
+  const modeChosen = useSystem
+    ? "system"
+    : mode === "dark" || mode === "light"
+    ? mode
+    : userMode;
+
+  if (useSystem) {
+    localStorage.removeItem("bs-theme");
+  } else {
+    localStorage.setItem("bs-theme", modeChosen);
+  }
+
+  document.documentElement.setAttribute(
+    "data-bs-theme",
+    useSystem ? (sysMode ? "light" : "dark") : modeChosen
+  );
+
+  document
+    .querySelectorAll(".mode-switch .btn-check")
+    .forEach((e) => (e.checked = false));
+  console.log("Mode: ", modeChosen);
+  document.getElementById(modeChosen).checked = true;
+}
+
 document.addEventListener("DOMContentLoaded", function (event) {
   console.log("Dom is Ready! Let's do some stuff");
+
+  setTheme();
+
   new ClipboardJS("#signature-copy");
 
   const copyButton = document.getElementById("signature-copy");
@@ -48,4 +79,16 @@ document.addEventListener("DOMContentLoaded", function (event) {
     input.addEventListener("input", handleInputChange);
   });
   copyButton.addEventListener("click", handleButtonClick);
+
+  document.querySelectorAll(".mode-switch .btn-check").forEach((radio) => {
+    radio.addEventListener("change", () => {
+      if (radio.checked) {
+        setTheme(radio.id);
+      }
+    });
+  });
+
+  window
+    .matchMedia("(prefers-color-scheme: light)")
+    .addEventListener("change", () => setTheme());
 });
